499f3b4c2a1240b63f09746ad328616364953be4
Apra24 <apra24@gmail.com>
2021-04-12 23:24:57 -0600

    fixed

--- A5T8MongoDB.py
+++ A5T8MongoDB.py
@@ -52,29 +52,36 @@ def handle_task():
     '''
     arguments for MongoDB query
     '''
-    match = {"$match": {"id": 16254}}
+    
+    user_input = "x"
+    
+    while (not user_input.isdigit()):
+        user_input = input("Please enter a listing ID: ")
+    user_input = int(user_input)
+    
+    match = {"$match": {"id": user_input}}
     unwind = {"$unwind": "$reviews" } 
-    project = {"$project": { "_id": 0, "host_name": 1, "price": 1, "reviews.comments": 1 }}
-    sort = {"$sort": { "reviews.date" : -1 }}
+    project = {"$project": { "_id": 0, "host_name": 1, "price": 1, "comments": "$reviews.comments", "date": "$reviews.date" }}
+    sort = {"$sort": { "date" : -1 }}
+    group = {"$group": {"_id": "$_id", "reviews": {"$push": {"host_name": "$host_name", "price": "$price", "reviews": "$reviews", "comments": "$comments", "date": "$date"}}}}
     limit = {"$limit": 1 }
     
     start_time = time.time()
 
     for i in range (0,20):
-        c = db.listings.aggregate([match,unwind,project,sort,limit])
+        c = db.listings.aggregate([match,unwind,project,sort,group,limit])
     
     q = ((time.time() - start_time)*1000)/20
     q = "{:.3f}".format(q)
-
-    print('*'*70)
 
     '''
     output the task result
     '''
     dict_array = list(c)
-    dict1 = dict_array[0]
+    
 
-    if (dict1["host_name"] != None):
+    if (len(dict_array) > 0):
+        dict1 = dict_array[0]["reviews"][0]
         print('*'*32)
         print('|  {:^9s}      | {:^9s}  |'.format('host_name','price'))
         print('-'*32)        
@@ -86,7 +93,7 @@ def handle_task():
     print('*'*32)
     print("Latest Review:")
     print()
-    printWrap(dict1["reviews"]["comments"])
+    printWrap(dict1["comments"])
     print()    
     
     
